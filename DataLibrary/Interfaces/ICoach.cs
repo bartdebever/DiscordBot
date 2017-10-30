08dcdde0c7eb2879587e9b9f86872ee7220e80b4
Unknown <bartdebever112@gmail.com>
2017-10-30 07:23:31 +0100

    Added ICoach and IGame

---
+++ DataLibrary/Interfaces/ICoach.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Interfaces
+{
+    /// <summary>
+    /// A coach is someone to teach another user about the ins and out of a game.
+    /// This might involve payment.
+    /// </summary>
+    public interface ICoach : IVoteable
+    {
+        /// <summary>
+        /// The user which is giving the coaching service.
+        /// Can be NULL
+        /// </summary>
+        IUser User { get; set; }
+        /// <summary>
+        /// The name of the coach.
+        /// </summary>
+        string Name { get; set; }
+        /// <summary>
+        /// The game which he specifies in.
+        /// </summary>
+        IGame Game { get; set; }
+    }
+}
---
+++ DataLibrary/Interfaces/IGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Interfaces
+{
+    /// <summary>
+    /// IGame is used to keep track of some games within our system.
+    /// This could be used to track owned games within our system or add them using the Discord "playing" feature.
+    /// </summary>
+    public interface IGame
+    {
+        /// <summary>
+        /// The name of the game the developers have given it.
+        /// </summary>
+        string Name { get; set; }
+        /// <summary>
+        /// A simple description about the game
+        /// </summary>
+        string Description { get; set; }
+    }
+}
