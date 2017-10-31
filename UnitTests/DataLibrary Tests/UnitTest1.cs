0a66e0dca7bfdb681890492fb733f087e3b1ab7d
Unknown <bartdebever112@gmail.com>
2017-10-31 09:41:23 +0100

    Added unittests to project

---
+++ UnitTests/DataLibrary Tests/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using DataLibrary.Discord.Implemented;
+using DataLibrary.Useraccounts.Implementation;
+using DataLibrary.Useraccounts.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.DataLibrary_Tests
+{
+    [TestClass]
+    public class UserTests
+    {
+        private List<ISummoner> summoners;
+        private IUser user;
+        [TestInitialize]
+        public void Initialize()
+        {
+            summoners = new List<ISummoner>();
+            user = new DiscordUser(0, "Bort", summoners);
+    }
+
+        [TestMethod]
+        public void FieldTest()
+        {
+            Assert.AreEqual("Bort", user.Name, "Name not saved correctly");
+            Assert.AreEqual(0, user.Id, "Id not saved correctly");
+            Assert.AreEqual(summoners, user.Summoners, "List not saved correctly");
+            Assert.AreEqual(0, user.Summoners.Count, "Summoner list already contains items");
+        }
+
+        [TestMethod]
+        public void AddSummoner()
+        {
+            ISummoner summoner = new APISummoner(0, "euw");
+            Assert.AreEqual(0, user.Summoners.Count, "List already contains summoners");
+            user.AddSummoner(summoner);
+            Assert.AreEqual(1, user.Summoners.Count, "Summoner not properly added");
+            Assert.AreEqual(0, user.Summoners[0].SummonerId, "Summoner Id incorrectly saved in list.");
+            Assert.AreEqual("euw", user.Summoners[0].Region, "Region incorrectly saved in list.");
+        }
+    }
+}
---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c0eca7dc-1158-42a1-ad05-f3c39a10d1be")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
