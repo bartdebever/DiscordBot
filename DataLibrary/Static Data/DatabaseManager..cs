f1cebae23d5936f413ad4230068dff4704f44561
Unknown <bartdebever112@gmail.com>
2017-11-08 12:23:51 +0100

    Added key generator for user.

--- DataLibrary/Static Data/DatabaseManager..cs
+++ DataLibrary/Static Data/DatabaseManager..cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataLibrary.Discord.Implemented;
 using DataLibrary.Useraccounts.Interfaces;
 
 namespace DataLibrary.Static_Data
@@ -14,6 +15,28 @@ namespace DataLibrary.Static_Data
         public static Mock GetMock()
         {
             return Mock;
-        } 
+        }
+
+        public static int UserKeyGenerator()
+        {
+            int next = 0;
+            lock (new object())
+            {
+                Random random = new Random(); //TODO Add random seed
+                bool validint = false;
+                while (!validint)
+                {
+                    DiscordUser user = null;
+                    next = random.Next(1, int.MaxValue - 1);
+                    user = Mock.Users.FirstOrDefault(x => x.Id == next);
+                    if (user != null)
+                    {
+                        validint = true;
+                    }
+                }
+            }
+            return next;
+
+        }
     }
 }
