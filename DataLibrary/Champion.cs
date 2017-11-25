cbffae8dbb5a201afd6fd59f95875dcc8eb6e0d5
Bart de Bever <bartdebever112@gmail.com>
2017-11-25 01:00:52 +0100

    Added lots of league options

--- DataLibrary/Champion.cs
+++ DataLibrary/Champion.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -47,6 +49,51 @@ namespace DataLibrary
         public string sanitizedTooltip { get; set; }
         public string name { get; set; }
         public string key { get; set; }
+        public List<vars> vars { get; set; }
+        public List<List<double>> effect { get; set; }
+        public List<double> range { get; set; }
+        public List<int> cost { get; set; }
+        [NotMapped]
+        public List<string> effectBurn {
+            get { return null; }
+            set
+            {
+                EffectBurn = "";
+                for (int i = 1; i < value.Count; i++)
+                {
+                    EffectBurn += value[i] + ",";
+                }; } }
+        public string EffectBurn { get; set; }
+    }
+
+    public class vars
+    {
+        public int id { get; set; }
+        [NotMapped]
+        public List<double> coeff {
+            get { return null; }
+            set {
+                Coeff = new List<coeff>();
+                foreach (var val in value)
+                {
+                    Coeff.Add(new coeff(val));
+                }
+            }
+
+        }
+        public List<coeff> Coeff { get; set; }
+        public string key { get; set; }
+    }
+
+    public class coeff
+    {
+        public coeff(double value)
+        {
+            this.value = value;
+        }
+        public coeff() { }
+        public int Id { get; set; }
+        public double value { get; set; }
     }
     public class StatsDto
     {
@@ -630,5 +677,148 @@ Zyra
         public string type { get; set; }
         public string version { get; set; }
         public Data data { get; set; }
+    }
+    /// <summary>
+    /// Baseclass that allows persisting of scalar values as a collection (which is not supported by EF 4.3)
+    /// </summary>
+    /// <typeparam name="T">Type of the single collection entry that should be persisted.</typeparam>
+    [ComplexType]
+    public abstract class PersistableScalarCollection<T> : ICollection<T>
+    {
+
+        // use a character that will not occur in the collection.
+        // this can be overriden using the given abstract methods (e.g. for list of strings).
+        const string DefaultValueSeperator = "|";
+
+        readonly string[] DefaultValueSeperators = new string[] { DefaultValueSeperator };
+
+        /// <summary>
+        /// The internal data container for the list data.
+        /// </summary>
+        private List<T> Data { get; set; }
+
+        public PersistableScalarCollection()
+        {
+            Data = new List<T>();
+        }
+
+        /// <summary>
+        /// Implementors have to convert the given value raw value to the correct runtime-type.
+        /// </summary>
+        /// <param name="rawValue">the already seperated raw value from the database</param>
+        /// <returns></returns>
+        protected abstract T ConvertSingleValueToRuntime(string rawValue);
+
+        /// <summary>
+        /// Implementors should convert the given runtime value to a persistable form.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        protected abstract string ConvertSingleValueToPersistable(T value);
+
+        /// <summary>
+        /// Deriving classes can override the string that is used to seperate single values
+        /// </summary>        
+        protected virtual string ValueSeperator
+        {
+            get
+            {
+                return DefaultValueSeperator;
+            }
+        }
+
+        /// <summary>
+        /// Deriving classes can override the string that is used to seperate single values
+        /// </summary>        
+        protected virtual string[] ValueSeperators
+        {
+            get
+            {
+                return DefaultValueSeperators;
+            }
+        }
+
+        /// <summary>
+        /// DO NOT Modeify manually! This is only used to store/load the data.
+        /// </summary>        
+        public string SerializedValue
+        {
+            get
+            {
+                var serializedValue = string.Join(ValueSeperator.ToString(),
+                    Data.Select(x => ConvertSingleValueToPersistable(x))
+                    .ToArray());
+                return serializedValue;
+            }
+            set
+            {
+                Data.Clear();
+
+                if (string.IsNullOrEmpty(value))
+                {
+                    return;
+                }
+
+                Data = new List<T>(value.Split(ValueSeperators, StringSplitOptions.None)
+                    .Select(x => ConvertSingleValueToRuntime(x)));
+            }
+        }
+
+        #region ICollection<T> Members
+
+        public void Add(T item)
+        {
+            Data.Add(item);
+        }
+
+        public void Clear()
+        {
+            Data.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return Data.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            Data.CopyTo(array, arrayIndex);
+        }
+
+        public int Count
+        {
+            get { return Data.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(T item)
+        {
+            return Data.Remove(item);
+        }
+
+        #endregion
+
+        #region IEnumerable<T> Members
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Data.GetEnumerator();
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Data.GetEnumerator();
+        }
+
+        #endregion
     }
 }
--- DiscordBot/Modules/LeagueModule.cs
+++ DiscordBot/Modules/LeagueModule.cs
@@ -26,18 +26,37 @@ namespace DiscordBot.Modules
         public async Task GetItem([Remainder] string name)
         {
             var database = new RiotData();
-            var item = database.Items.FirstOrDefault(x => x.name.ToLower().Contains(name.ToLower()));
-            var builder = Builders.BaseBuilder("", "", Color.DarkBlue, new EmbedAuthorBuilder().WithName(item.name),
-                $"http://ddragon.leagueoflegends.com/cdn/6.24.1/img/item/{item.ItemId}.png");
-            builder.AddField("Description", Regex.Replace(item.description.Replace("<br>", "\n"), "<.*?>", String.Empty));
-            builder.AddField("Effect", item.plaintext);
-            builder.AddField("Cost", $"**Total: **{item.gold.total}\n" +
-                                     $"**Base: **{item.gold.Base}");
-            if (!string.IsNullOrEmpty(item.requiredChampion))
+            var item = database.Items.FirstOrDefault(x => x.name.ToLower().Equals(name.ToLower()));
+            if (item == null)
             {
-                builder.AddField("Required Champion", item.requiredChampion);
+                var items = database.Items.Where(x => x.name.ToLower().Contains(name.ToLower())).ToList();
+                var builder = Builders.BaseBuilder("Multiple items found", "", Color.Red, null, "");
+                string itemsstring = "";
+                items.ForEach(x=> itemsstring += x.name +"\n");
+                builder.AddField("Items", itemsstring);
+                await ReplyAsync("", embed: builder.Build());
             }
-            await ReplyAsync("", embed: builder.Build());
+            else
+            { 
+                var builder = Builders.BaseBuilder("", "", Color.DarkBlue, new EmbedAuthorBuilder().WithName(item.name),
+                    $"http://ddragon.leagueoflegends.com/cdn/6.24.1/img/item/{item.ItemId}.png");
+                builder.AddField("Effect", item.plaintext);
+                builder.AddField("Tooltip",
+                    Regex.Replace(item.description.Replace("<br>", "\n"), "<.*?>", String.Empty));
+                builder.AddField("Cost", $"**Total: **{item.gold.total}\n" +
+                                         $"**Base: **{item.gold.Base}");
+                if (!string.IsNullOrEmpty(item.requiredChampion))
+                {
+                    builder.AddField("Required Champion", item.requiredChampion);
+                }
+                if (item.consumed)
+                {
+                    builder.AddField("Consumable", $"This item is a consumable and a play can have {item.stacks} of this item at a time.");
+                }
+                await ReplyAsync("", embed: builder.Build());
+
+            }
+
 
         }
 
@@ -52,8 +71,11 @@ namespace DiscordBot.Modules
         public async Task Champion([Remainder] string name)
         {
             var database = new RiotData();
-            List<DataLibrary.Champion> champions = database.Champions.Include("spells").Include("skins").Include("passive").ToList();
-            var champ = champions.FirstOrDefault(x => x.name.ToLower().Contains(name.ToLower()));
+            List<DataLibrary.Champion> champions = database.Champions.Include("spells").Include("skins").Include("passive").Include("stats").ToList();
+            DataLibrary.Champion champ = null;
+            champ = champions.FirstOrDefault(x => x.name.ToLower().Equals(name.ToLower()));
+            if(champ == null)
+            champ = champions.FirstOrDefault(x => x.name.ToLower().Contains(name.ToLower()));
             var builder = Builders.BaseBuilder(champ.name, champ.title, Color.DarkBlue, null, $"http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/{champ.key}.png");
             //builder.AddField("Stats", $"**Dificulty: **{champ.stats.difficulty}\n" +
             //                          $"**Attack: **{champ.stats.attack}\n" +
@@ -70,6 +92,97 @@ namespace DiscordBot.Modules
             string skins = "";
             champ.skins.ForEach(x=> skins += x.name + "\n");
             builder.AddField("Skins", skins);
+            await ReplyAsync("", embed: builder.Build());
+        }
+
+        [Command("skin")]
+        public async Task Skin([Remainder] string name)
+        {
+            var database= new RiotData();
+            var skins = database.Skins;
+            var skinlist = skins.Where(x => x.name.ToLower().Contains(name.ToLower())).ToList();
+            if (skinlist.Count() > 1)
+            {
+                var builder = Builders.BaseBuilder("Error: Multiple found", "", Color.Red, null, "");
+                string doubes = "";
+                skinlist.ForEach(x=> doubes += x.name + "\n");
+                builder.AddField("Skins", doubes);
+                await ReplyAsync("", embed: builder.Build());
+            }
+            else
+            {
+                var skin = skinlist[0];
+                Champion champ = null;
+                foreach (var databaseChampion in database.Champions.Include("skins"))
+                {
+                    foreach (var databaseChampionSkin in databaseChampion.skins)
+                    {
+                        if (databaseChampionSkin.id == skin.id)
+                        {
+                            champ = databaseChampion;
+                        }
+                    }
+                }
+                var builder = Builders.BaseBuilder("", "", Color.DarkBlue, null, "");
+                builder.WithImageUrl($"http://ddragon.leagueoflegends.com/cdn/img/champion/splash/{champ.key}_{skin.num}.jpg");
+                await ReplyAsync("", embed: builder.Build());
+            }
+        }
+
+        [Command("skill")]
+        public async Task Skill([Summary("Champion name")]string champion, [Summary("Ability used")] string ability)
+        {
+            var database = new RiotData();
+            List<DataLibrary.Champion> champions = database.Champions.Include("spells").Include("skins").Include("passive").Include("stats").Include("spells.vars").Include("spells.vars.Coeff").ToList();
+            DataLibrary.Champion champ = null;
+            champ = champions.FirstOrDefault(x => x.name.ToLower().Equals(champion.ToLower()));
+            if (champ == null)
+                champ = champions.FirstOrDefault(x => x.name.ToLower().Contains(champion.ToLower()));
+            var keys = new List<string>() { "Q", "W", "E", "R" };
+            int index = keys.IndexOf(ability.ToUpper());
+            var spell = champ.spells[index];
+            var builder = Builders.BaseBuilder(spell.name, "", Color.DarkBlue,
+                new EmbedAuthorBuilder().WithName(champ.name + " " + ability), $"http://ddragon.leagueoflegends.com/cdn/6.24.1/img/spell/{spell.key}.png ");
+            builder.AddField("Description", spell.sanitizedDescription);
+            string tooltip = spell.sanitizedTooltip;
+
+            foreach (var spellVar in spell.vars)
+            {
+                tooltip = tooltip.Replace("{{ "+spellVar.key+" }}", spellVar.Coeff[0].value.ToString());
+            }
+            var spellburn = spell.EffectBurn.Split(',').ToList();
+            for (int i = 1; i < spellburn.Count; i++)
+            {
+                tooltip = tooltip.Replace("{{ e" + i + " }}", spellburn[i]);
+            }
+
+            builder.AddField("Tooltip",tooltip );
+            builder.AddField("Stats", $"**Resource: **{spell.costType}\n" +
+                                      $"**Costs: **{spell.resource.Replace("{{ cost }}", "40")}\n" +
+                                      $"**Cooldown: **{spell.cooldownBurn}");
+            
+            await ReplyAsync("", embed: builder.Build());
+        }
+
+        [Command("game")]
+        public async Task Game(string region, [Remainder] string summonername)
+        {
+            var builder = Builders.BaseBuilder("", "", Color.DarkBlue,
+                new EmbedAuthorBuilder().WithName(summonername + "'s game"), "");
+            builder.AddField("Bans",
+                "**Team1: **\nChampion1, Champion2, Champion3\n**Team2: **\nChampion1, Champion2, Champion3");
+            builder.AddField("Team 1",
+                "Name1: Champion1 | Bronze5\n" +
+                "Name2: Champion2 | Bronze5\n" +
+                "Name3: Champion3 | Bronze5\n" +
+                "Name4: Champion4 | Bronze5\n" +
+                "Name5: Champion6 | Bronze5");
+            builder.AddField("Team 2",
+                "Name1: Champion1 | Bronze5\n" +
+                "Name2: Champion2 | Bronze5\n" +
+                "Name3: Champion3 | Bronze5\n" +
+                "Name4: Champion4 | Bronze5\n" +
+                "Name5: Champion6 | Bronze5");
             await ReplyAsync("", embed: builder.Build());
         }
     }
