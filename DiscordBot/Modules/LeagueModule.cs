9af92b1bc17144db1da9ccf7fa87dabb914cb3ed
Bart de Bever <bartdebever112@gmail.com>
2017-12-04 23:19:49 +0100

    Added RiotWrapper data to league module

--- DiscordBot/Modules/LeagueModule.cs
+++ DiscordBot/Modules/LeagueModule.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ChampionGGHandler;
 using DataLibrary;
+using DataLibrary.Static_Data;
 using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
 using DiscordBot.EmbedBuilder;
+using RiotWrapper;
+using RiotWrapper.DataTypes;
 
 namespace DiscordBot.Modules
 {
@@ -165,40 +170,119 @@ namespace DiscordBot.Modules
         }
 
         [Command("game")]
-        public async Task Game(string region, [Remainder] string summonername)
+        public async Task Game(string region, [Remainder] string summonerName)
         {
+            Platforms platform = (Platforms)Enum.Parse(typeof(Platforms), region.ToUpper());
+            var riotClient = new RiotClient(OptionManager.RiotKey);
+            var champions = new RiotData().Champions;
+            var summoner = riotClient.Summoner.GetSummonerByName(summonerName, platform);
+            var match = riotClient.Specate.GameBySummoner(platform, summoner.SummonerId);
             var builder = Builders.BaseBuilder("", "", Color.DarkBlue,
-                new EmbedAuthorBuilder().WithName(summonername + "'s game"), "");
-            builder.AddField("Information", "**Map: **Rift\n**Time: **30:30\n**Mode: **Normals");
-            builder.AddField("Bans Team1",
-                "Champion1, Champion2, Champion3, Champion4, Champion5");
-            builder.AddInlineField("Summoner", "Name1\nname2\nname3\nname4\nname5");
-            builder.AddInlineField("Champion", "champ1\nchamp2\nchamp3\nchamp4\nchamp5");
-            builder.AddInlineField("Rank","rank5\nrank5\nrank5\nrank5\nrank5");
-            builder.AddField("Bans Team2",
-                "Champion1, Champion2, Champion3, Champion4, Champion5");
-            builder.AddInlineField("Summoner", "Name1\nname2\nname3\nname4\nname5");
-            builder.AddInlineField("Champion", "champ1\nchamp2\nchamp3\nchamp4\nchamp5");
-            builder.AddInlineField("Rank", "rank5\nrank5\nrank5\nrank5\nrank5");
+                new EmbedAuthorBuilder().WithName(summonerName + "'s game"), "");
+            TimeSpan time = new TimeSpan(0, 0, (int) match.GameLength);
+            builder.AddField($"Information", $"**Map: **{match.MapId}\n**Time: **{Math.Round(time.TotalMinutes,2)} minutes\n**Mode: **{match.GameMode}");
+            for (int i = 1; i < 3; i++)
+            {
+                string bans1 = "";
+                foreach (var matchBannedChampion in match.BannedChampions)
+                {
+                    if (matchBannedChampion.TeamId == i*100)
+                    {
+                        try
+                        {
+                            bans1 +=
+                                champions.FirstOrDefault(x => x.ChampionId == matchBannedChampion.ChampionId).name +
+                                ", ";
+                        }
+                        catch
+                        {
+                            bans1 += "None, ";
+                        }
+                    }
+                }
+                bans1 = bans1.Remove(bans1.Length - 2, 2);
+                builder.AddField("Bans Team "+i,
+                    bans1);
+                string names = "";
+                string championsNames = "";
+                foreach (var currentGameParticipant in match.Participants.Where(x => x.TeamId == i*100).ToList())
+                {
+                    names += currentGameParticipant.SummonerName + "\n";
+                    championsNames += champions.FirstOrDefault(x=> x.ChampionId == currentGameParticipant.ChampionId).name + "\n";
+                }
+                builder.AddInlineField("Summoners", names);
+                builder.AddInlineField("Champion", championsNames);
+            }
             await ReplyAsync("", embed: builder.Build());
         }
-
         [Command("summoner")]
         public async Task Profile(string region, [Remainder] string summonerName)
         {
-            var builder = Builders.BaseBuilder("", "", Color.DarkBlue, new EmbedAuthorBuilder().WithName(summonerName),
-                "");
-            builder.AddInlineField("Information", $"**Name: **name\n**Region: **region\n**Level: **level");
-            builder.AddInlineField("Ranking", $"**Solo/Duo: **rank V\n**Flex: **rank V\n**3v3: **rank V");
-            builder.AddField("Top Mastery Champions", "The top 3 champions by mastery score");
-            builder.AddInlineField("Champ1", "**Level: **X\n**Score: **XXXXXXXXXX");
-            builder.AddInlineField("Champ2", "**Level: **X\n**Score: **XXXXXXXXXX");
-            builder.AddInlineField("Champ3", "**Level: **X\n**Score: **XXXXXXXXXX");
+            var riotClient = new RiotClient(OptionManager.RiotKey);
+            Platforms platform = (Platforms) Enum.Parse(typeof(Platforms), region.ToUpper());
+            var summoner = riotClient.Summoner.GetSummonerByName(summonerName,platform);
+            var leagues = riotClient.League.GetPositionDto(platform, summoner.SummonerId);
+            var masteries = riotClient.Masteries.GetchampionMasteries(platform, summoner.SummonerId);
+            var matches = 
+            masteries = masteries.OrderByDescending(x => x.Level).ThenByDescending(x=> x.ChampionPoints).ToList();
+            var builder = Builders.BaseBuilder("", "", Color.DarkBlue, new EmbedAuthorBuilder().WithName(summoner.Name),
+                $"http://ddragon.leagueoflegends.com/cdn/6.24.1/img/profileicon/{summoner.ProfileIconId}.png");
+            builder.AddInlineField($"Information", $"**Name: **{summoner.Name}\n**Region: **region\n**Level: **{summoner.SummonerLevel}");
+            string rankings = "";
+            foreach (var leaguePositionDto in leagues)
+            {
+                rankings += $"**{leaguePositionDto.QueueType}: **{leaguePositionDto.Tier} {leaguePositionDto.Rank}\n";
+            }
+            if (rankings == "") rankings = "Unranked";
+            builder.AddInlineField($"Ranking", rankings);
+            int champions = 4;
+            if (champions > masteries.Count) champions = masteries.Count;
+            builder.AddField("Top Mastery Champions", $"The top {champions} champions by mastery score");
+            for (int i = 0; i < champions; i++)
+            {
+                int id = Convert.ToInt32(masteries[i].ChampionId);
+                var champion = new RiotData().Champions.FirstOrDefault(x=> x.ChampionId == id);
+                builder.AddInlineField(champion.name, $"**Level: **{masteries[i].Level}\n**Points: **{masteries[i].ChampionPoints}");
+            }
             builder.AddField("Top Ranked Champions", "The top 3 champions by ranked played games");
             builder.AddInlineField("Champ1", "**Games: **X\n**Average Stats:** K/D/A\n**Winrate: **XX%");
             builder.AddInlineField("Champ2", "**Games: **X\n**Average Stats:** K/D/A\n**Winrate: **XX%");
             builder.AddInlineField("Champ3", "**Games: **X\n**Average Stats:** K/D/A\n**Winrate: **XX%");
+            builder.AddInlineField("Champ4", "**Games: **X\n**Average Stats:** K/D/A\n**Winrate: **XX%");
             await ReplyAsync("", embed: builder.Build());
+        }
+
+        [Command("mastery")]
+        public async Task Mastery(string region, string summonerName, string championName)
+        {
+            RiotData data = new RiotData();
+            DataLibrary.Champion champion = null;
+            Discord.EmbedBuilder builder = null;
+            var championList = data.Champions.Where(x=> x.name.ToLower().Equals(championName.ToLower())).ToList();
+            if (championList.Count < 1)
+            {
+                championList = new RiotData().Champions.Where(x => x.name.ToLower().Contains(championName.ToLower()))
+                    .ToList();
+            }
+            champion = championList[0];
+            if (champion != null)
+            {
+                builder = Builders.BaseBuilder($"{summonerName} mastery for {champion.name}", "", Color.DarkBlue, null, $"http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/{champion.key}.png");
+                builder.AddInlineField("Level", "X");
+                builder.AddInlineField("Points", "XXXXXXXXXXXX");
+            }
+            else
+            {
+                builder = Builders.ErrorBuilder("Champion was not found");
+            }
+
+            await ReplyAsync("", embed: builder.Build());
+        }
+
+        [Command("mastery")]
+        public async Task Mastery(string champion)
+        {
+            await ReplyAsync("Only using build in account");
         }
     }
 }
