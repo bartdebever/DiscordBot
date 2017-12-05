778860df02945ca6d5f539b66a021260a177f2b6
Bart de Bever <bartdebever112@gmail.com>
2017-12-05 17:37:31 +0100

    Added string to platform helper

---
+++ RiotWrapper/Helpers/PlatformHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RiotWrapper.DataTypes;
+
+namespace RiotWrapper.Helpers
+{
+    public static class PlatformHelper
+    {
+        public static Platforms StringToPlatform(string region)
+        {
+            region = region.ToLower();
+            if (region == "eune")
+            {
+                return Platforms.EUN1;
+            }
+            else if (region == "las")
+            {
+                return Platforms.LA2;
+            }
+            else if (region == "lan")
+            {
+                return Platforms.LA1;
+            }
+            else if (region == "OCE")
+            {
+                return Platforms.OC1;
+            }
+
+
+            
+            region = region + "1";
+            return (Platforms) Enum.Parse(typeof(Platforms), region);
+        }
+    }
+}
